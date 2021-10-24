c8d353a86f73ed9d04ef608ed7ca5edec94fb321
Badly89 <malykhinal@mail.ru>
2021-10-24 16:22:57 +0000

    отправка сообщения + ответ робота, без сохранения в БД. fix iss-4, iss-3

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -1,5 +1,28 @@
 import React from "react";
-const Main = () => {
-    return <h3>в разработке</h3>;
+import { LeftSideBar } from "./SideBar/leftSideBar";
+import { ChatList } from "./ChatList/chatList";
+import { FieldMessage } from "./FieldMessage/fieldMessage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+export const MainWindow = () => {
+    return (
+        <>
+            <div className="main-window">
+                {/* <Switch>
+                    <Route exact path="/chats">
+                        <ChatList />
+                    </Route>
+                    <Route exact path="/profile">
+                        <UserProfile />
+                    </Route>
+                    <Route exact path="/friends">
+                        <FriendsList />
+                    </Route>
+                    <Route exact path="/calls">
+                        <CallList />
+                    </Route>
+                </Switch> */}
+            </div>
+        </>
+    );
 };
-export default Main;
