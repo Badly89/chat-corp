023249e7470006bc8e90556ec30178c7b3ac9506
Mikhail <nickiforov.mihail2017@yandex.ru>
2021-11-12 18:19:06 +0300

    Password generator for email

--- app/Mail/ForgotPassword.php
+++ app/Mail/ForgotPassword.php
@@ -2,10 +2,13 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ForgotPassword extends Mailable
 {
@@ -19,9 +22,17 @@ class ForgotPassword extends Mailable
      */
     public function __construct($request)
     {
+        $new_password = Str::random(10);
+
+        $user = User::firstWhere('email', $request->email);
+        $user->password = Hash::make($new_password);
+        $user->save();
+
         $this->details = [
             'title' => 'Mail from Chat Corp',
-            'body' => $request->email
+            'body' => $request->email,
+            'password' => $new_password,
+
         ];
     }
 
