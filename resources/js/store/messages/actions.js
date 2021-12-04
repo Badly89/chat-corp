1d48d0ed2a7783dfed5c804aadf33c9873b60a89
Mikhail <nickiforov.mihail2017@yandex.ru>
2021-12-04 20:23:58 +0300

    'create_channels'

--- resources/js/store/messages/actions.js
+++ resources/js/store/messages/actions.js
@@ -4,16 +4,10 @@ import {
     GET_MESSAGES_FAIL,
     GET_MESSAGES_REQUEST,
     GET_MESSAGES_SUCCESS,
-    SEND_MESSAGE,
-    UPDATE_MESSAGES,
 } from "./types";
 
 import axios from "axios";
 
-export const sendMessage = (channel_id, content) => ({
-    type: SEND_MESSAGE,
-    payload: { content, channel_id },
-});
 export const loadMessages = (messages) => ({
     type: GET_MESSAGES_REQUEST,
     payload: messages,
@@ -39,41 +33,11 @@ export const getMessagesChannel = (channel_id) => (dispatch, getState) => {
                 console.log(res.data);
                 dispatch(loadMessages(res.data));
 
-                // dispatch({ type: GET_MESSAGES_SUCCESS });
+                dispatch({ type: GET_MESSAGES_SUCCESS });
                 // });
             })
             .catch((err) => {
-                // dispatch({ type: GET_MESSAGES_FAIL });
+                dispatch({ type: GET_MESSAGES_FAIL });
             });
     }
 };
-
-export const updateMessages = (channel_id, content) => (dispatch, getState) => {
-    console.log("Обновляем сообщения");
-};
-export const actionMessage = (channel_id, content) => (dispatch, getState) => {
-    const body = JSON.stringify({ channel_id, content });
-
-    // dispatch(sendMessage(content));
-    console.log(body);
-    // console.log(lrc_token);
-
-    axios
-        .post("/sendMessage", body, {
-            withCredentials: true,
-        })
-        .then((res) => {
-            console.log(res);
-            dispatch(getMessagesChannel(channel_id));
-        })
-        .catch((err) => {
-            const errors = err.response.data.errors;
-            console.log(errors);
-        });
-};
-
-export const actionDelMessage =
-    (channel_id, content) => (dispatch, getState) => {
-        dispatch(delMessage(channel_id, content));
-        const selMessage = getState().content;
-    };
